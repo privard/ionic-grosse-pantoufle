3a2180510d1f4e07e164a2e96767e5cefb504ea3
Philippe Vachon-Rivard <Philippe.Vachon-Rivard@iprospect.com>
2014-09-18 11:55:21 -0400

    testing on tap versus on click

--- www/js/app.js
+++ www/js/app.js
@@ -75,6 +75,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.filters'])
       }
     });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/pantoufles');
 });
 
